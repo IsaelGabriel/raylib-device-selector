f050207d3d12be8b9a1e3b9209ce645318a3975b
IsaelGSF <isaelgsfaria@gmail.com>
2025-02-25 13:47:34 -0300

    Create window

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Raylib_cs;
+
+static class Game {
+    const int Width = 600;
+    const int Height = 600;
+    const string Title = "Device Selector";
+
+    const int TargetFPS = 60;
+
+    static readonly Color ClearColor = Color.DarkPurple;
+
+    static void Main() {
+        Raylib.InitWindow(Width, Height, Title);
+
+        Raylib.SetTargetFPS(TargetFPS);
+
+        while(!Raylib.WindowShouldClose()) { 
+            Raylib.ClearBackground(ClearColor);
+            Render();
+        }
+
+        Raylib.CloseWindow();
+    }
+
+    static void Render() {
+        Raylib.BeginDrawing();
+            Raylib.DrawFPS(0,0);
+        Raylib.EndDrawing();
+    }
+}
